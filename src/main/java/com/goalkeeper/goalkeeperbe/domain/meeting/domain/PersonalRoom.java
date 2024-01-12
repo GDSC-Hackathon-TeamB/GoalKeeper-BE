7134ddcecc86d5b723c58a849347455e27bc2509
DongHoon Lee <dhl1924@naver.com>
2024-01-13 07:44:03 +0900

    feat: PersonalRoom Entity 생성

--- src/main/java/com/goalkeeper/goalkeeperbe/domain/meeting/domain/PersonalRoom.java
+++ src/main/java/com/goalkeeper/goalkeeperbe/domain/meeting/domain/PersonalRoom.java
@@ -1,6 +1,6 @@
 package com.goalkeeper.goalkeeperbe.domain.meeting.domain;
 
-import com.goalkeeper.goalkeeperbe.domain.users.domain.Users;
+import com.goalkeeper.goalkeeperbe.domain.users.domain.User;
 import com.goalkeeper.goalkeeperbe.global.shared.domain.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -9,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,7 +21,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "personal_room")
 public class PersonalRoom extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,11 +30,11 @@ public class PersonalRoom extends BaseTimeEntity {
 
     @ManyToOne
     @JoinColumn(name = "user1_id", nullable = false)
-    private Users user1;
+    private User user1;
 
     @ManyToOne
     @JoinColumn(name = "user2_id", nullable = false)
-    private Users user2;
+    private User user2;
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
