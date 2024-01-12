6b8cfaa632b32636b25c2665e69f290dee63de1b
DongHoon Lee <dhl1924@naver.com>
2024-01-13 02:30:59 +0900

    refactor: 클래스의 설정을 기반으로 컨텍스트 로드

--- src/test/java/com/goalkeeper/goalkeeperbe/GoalKeeperBeApplicationTests.java
+++ src/test/java/com/goalkeeper/goalkeeperbe/GoalKeeperBeApplicationTests.java
@@ -3,7 +3,7 @@ package com.goalkeeper.goalkeeperbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GoalKeeperBeApplicationTests.class)
 class GoalKeeperBeApplicationTests {
 
 	@Test
