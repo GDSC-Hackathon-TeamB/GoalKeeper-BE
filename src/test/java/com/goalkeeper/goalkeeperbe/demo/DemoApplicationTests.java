1100500a39ee748a2e1a210dc120e28b8f6c4570
eunseolim <eunseolim1018@gmail.com>
2024-01-12 21:37:46 +0900

    feat : initial commit

---
+++ src/test/java/com/goalkeeper/goalkeeperbe/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goalkeeper.goalkeeperbe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
