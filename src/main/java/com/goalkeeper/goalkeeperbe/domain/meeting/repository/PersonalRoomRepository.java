735ac10b93518cb71ef877218bc6c311c4825764
DongHoon Lee <dhl1924@naver.com>
2024-01-13 07:41:20 +0900

    feat: PersonalRoom 레포지토리 생성

--- src/main/java/com/goalkeeper/goalkeeperbe/domain/meeting/repository/PersonalRoomRepository.java
+++ src/main/java/com/goalkeeper/goalkeeperbe/domain/meeting/repository/PersonalRoomRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PersonalRoomRepository extends JpaRepository<PersonalRoom, Long> {
-    Optional<PersonalRoom> findByRoomId(Long roomId);
+    Optional<PersonalRoom> findByPersonalRoomId(Long personalRoomId);
 
-    @Query("SELECT pr FROM PersonalRoom pr WHERE (pr.user1.userId = :userId OR pr.user2.userId = :userId)")
+    @Query("SELECT pr FROM personal_room pr WHERE (pr.user1.userId = :userId OR pr.user2.userId = :userId)")
     Optional<PersonalRoom> findPersonalRoomByUserId(@Param("userId") Long userId);
 }
